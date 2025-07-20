96c886b2d42d1601d102aa0ac66e9669f7493d1f
ayush arya <ayusharya967@gmail.com>
2025-07-20 21:51:01 +0530

    minor bugs fixed

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ Answer:
 
 # --- Step 8: Run server locally ---
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
